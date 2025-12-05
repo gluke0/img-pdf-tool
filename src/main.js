eb9eba1dc85d5577312a55f0bd6816655c02386e
gluke0 <milanesigianluca@gmail.com>
2025-12-05 12:17:40 +0100

    bootstrap removed

--- src/main.js
+++ src/main.js
@@ -2,8 +2,4 @@ import { createApp } from 'vue'
 // import './style.css'
 import App from './App.vue'
 
-//importare bootstrap js
-import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
-
-
 createApp(App).mount('#app')
