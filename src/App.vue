2651776120be4b69273f1533f9fb1e3c27ea7843
gluke0 <milanesigianluca@gmail.com>
2025-12-06 18:04:57 +0100

    main elements of the structure

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,26 @@
-<script >
+<script>
+  import Header from './components/header.vue';
+  import Main from './components/main.vue';
+  import Footer from './components/footer.vue';
 
+  export default{
+    name: "App",
+    data(){
+      return{
+      }
+    },
+  components:{
+    Header,
+    Main,
+    Footer,
+  },
+};
 </script>
 
 <template>
-  
+  <Header></Header>
+  <Main></Main>
+  <Footer></Footer>
 </template>
 
 <style lang="scss">
---
+++ src/components/header.vue
@@ -0,0 +1,20 @@
+<script>
+
+export default{
+   name: "Header",
+   data(){
+      return{
+      }
+   },
+   components:{
+   }
+};
+</script>
+
+<template>
+
+</template>
+
+<style lang="scss">
+@use '../style/main.scss';
+</style>
