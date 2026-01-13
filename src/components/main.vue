c845186084392f59e1086e244d44e24bc253ef33
gluke0 <milanesigianluca@gmail.com>
2026-01-13 17:08:49 +0100

    main div

--- src/components/main.vue
+++ src/components/main.vue
@@ -12,7 +12,16 @@ export default{
 </script>
 
 <template>
-
+   <div class="main-div">
+      <input type="file" accept="image/*" @change="onFileChange"/>
+      <div v-if="imageDataUrl">
+         <h3>Preview</h3>
+         <img :src="imageDataUrl" alt="preview" style="max-width: 300px;"/>
+      </div>
+      <button v-if="imageDataUrl" @click="downloadPdf">
+      Download PDF
+      </button>
+  </div>
 </template>
 
 <style lang="scss">
