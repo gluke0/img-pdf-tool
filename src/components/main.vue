14078b71e5ea9b456068517ac581da10eab712c6
gluke0 <milanesigianluca@gmail.com>
2026-01-20 14:46:47 +0100

    fixing the bew download - asyinc function

--- src/components/main.vue
+++ src/components/main.vue
@@ -1,53 +1,50 @@
 <script>
-export default {
+export default{
   name: 'Main',
-  data() {
-    return {
+  data(){
+    return{
       imageDataUrl: null,
-      isGrayscale: false, // b/w?
-    };
+      isGrayscale: false,
+    }
   },
-  methods: {
-    onFileChange(event) {
-      const file = event.target.files[0];
-      if (!file) return;
+  methods:{
+    onFileChange(event){
+      const file = event.target.files[0]
+      if (!file) return
+
       window.imageToDataUrl(file, (dataUrl) =>{
-        this.imageDataUrl = dataUrl;
-      });
+        this.imageDataUrl = dataUrl
+      })
     },
 
-    downloadPdf(){
-      if (!this.imageDataUrl) return;
-      window.generatePdfFromImage(this.imageDataUrl, this.isGrayscale);
+    async downloadPdf(){
+      if (!this.imageDataUrl) return
+      // pass data url and grayscale if there
+      await window.generatePdfFromImage(this.imageDataUrl, this.isGrayscale)
     },
   },
-};
+}
 </script>
 
 <template>
   <div class="main-div">
-    <input type="file" accept="image/*" @change="onFileChange"/>
+    <input type="file" accept="image/*" @change="onFileChange" />
     <div class="preview-div" v-if="imageDataUrl">
       <h3>Preview</h3>
-      <img 
-        :src="imageDataUrl" 
-        alt="preview"
-        :style="{filter: isGrayscale ? 'grayscale(100%)' : 'none'}"/>
+      <img :src="imageDataUrl" alt="preview" :style="{ filter: isGrayscale ? 'grayscale(100%)' : 'none' }"/>
       <div class="bew">
-        <div class="bew">
-          <input type="checkbox" v-model="isGrayscale"/>
-          <span class="bew-text">Black and white</span>
-        </div>
+        <input type="checkbox" v-model="isGrayscale" />
+        <span class="bew-text">Black and white</span>
       </div>
     </div>
-    <button v-if="imageDataUrl" @click="downloadPdf">
-      <i class="fa-solid fa-download"></i>
-      <span class="download-word">Download</span>
-      <span>PDF</span>
-    </button>
+    <div v-if="imageDataUrl" @click="downloadPdf">
+      <button class="download-div">
+        <span>Download PDF</span>
+      </button>
+    </div>
   </div>
 </template>
 
 <style lang="scss">
-@use '../style/main.scss';
+@import '../style/main.scss';
 </style>
